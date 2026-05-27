bf193aa3f10e222500962076e59e710f0a0818df
Dogukan EFE <sdgknefe@gmail.com>
2026-05-28 01:01:49 +0300

    Add FASTA parser(io module for sequence parsing)

---
+++ src/io/fasta_parser.py
@@ -0,0 +1,33 @@
+'''
+    : FASTA PARSER :
+'''
+def fasta_parser(path_file: str) -> dict:
+
+    data = {}
+    header = None
+    sequence = ""
+
+    with open(path_file) as file:
+        
+        for line in file:
+            line = line.strip()
+            
+            if not line:
+                continue
+            
+            elif line.startswith(">"):
+                
+                if header != None:
+                    data[header] = sequence
+                
+                header = line[1:]
+                sequence = ""
+
+            else:
+                sequence += line
+
+        if header is not None:
+            data[header] = sequence
+
+    return data
+
